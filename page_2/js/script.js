4a7206e35121ab9f883304a222b0dc40968db1ab
dallas-bean <146159176+dallas-bean@users.noreply.github.com>
2023-12-05 14:29:08 -0800

    Update script.js

--- page_2/js/script.js
+++ page_2/js/script.js
@@ -10,6 +10,28 @@ function closeout3() {
     var reset = document.getElementById("warning3");
     reset.style.display = "none";
 }
+function closeout4() {
+    var reset = document.getElementById("warning4");
+    reset.style.display = "none";
+}
+function closeout5() {
+    var resetOne = document.getElementById("picStandards");
+    var myTurn = document.getElementById("sicStandards");
+    resetOne.style.display = "none";
+    myTurn.style.display = "block";
+}
+function closeout6() {
+    var resetOne = document.getElementById("sicStandards");
+    var myTurn = document.getElementById("picStandards");
+    resetOne.style.display = "none";
+    myTurn.style.display = "block";
+}
+function closeout7() {
+    var resetOne = document.getElementById("sicStandards");
+    var resetTwo = document.getElementById("picStandards");
+    resetOne.style.display = "none";
+    resetTwo.style.display = "none";
+}
 // 
 function popup() {
     var checkbox = document.getElementById("q1");
@@ -41,6 +63,25 @@ function popup2() {
                 prompt.style.display = "none"
             }
         }
+    function popup4() {
+        var checkbox = document.getElementById("q4");
+        var prompt = document.getElementById("warning4");
+    
+        if (checkbox.checked == true){
+            prompt.style.display = "block";}
+            else {
+                prompt.style.display = "none"
+            }
+        }
+        function popup5() {
+            var clickable = document.getElementById("crewStandardsPopup");
+            clickable.addEventListener("click", showMe());
+        
+            function showMe() {
+                var one = document.getElementById("picStandards");
+                one.style.display = "block";   
+            }
+        }
 // 
 function one() {
     var element = document.getElementById('one');
@@ -48,5 +89,9 @@ function one() {
 }
 function two() {
     var element = document.getElementById('two');
+    element.classList.toggle("collapse");
+}
+function three() {
+    var element = document.getElementById('ejm');
     element.classList.toggle("collapse");
 }
