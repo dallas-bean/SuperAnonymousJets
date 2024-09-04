03c3e0ef77f6c27293c4d5607665ec72a238b7f2
Dallas Bean <174537881+dbeanpro@users.noreply.github.com>
2024-09-04 16:28:27 -0700

    redid home hub. Now publishing finished product.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,12 @@
-function unhide(buttonId, divId) {
-    const divs = document.querySelectorAll('.navToggle');
+function reveal(targetID) {
+    let target = document.getElementById(targetID);
+    const subMenus = document.querySelectorAll('.submenu');
 
-    divs.forEach((div) => {
-        if (div.id !== divId && !div.classList.contains("collapse")) {
-            div.classList.add("collapse");
+    subMenus.forEach(submenu => {
+        if (submenu.id !== targetID) {
+            submenu.classList.add('hidden');
         }
     });
 
-    const displayMe = document.getElementById(divId);
-    const hidden = displayMe.classList.contains("collapse");
-
-    divs.forEach((div) => {
-        if (div.id !== divId && !hidden) {
-            div.classList.add('collapse');
-            div.classList.remove('fadeIn')
-        }
-    });
-
-    displayMe.classList.toggle("collapse", !hidden);
-}
-
-function fade() {
-    document.getElementById("portals").classList.toggle("fadeIn");
-}
-
-function fade1() {
-    document.getElementById("resources").classList.toggle("fadeIn");
-}
-
-function phones() {
-    document.getElementById("phones").classList.toggle("collapse");
+    target.classList.toggle("hidden");
 }
